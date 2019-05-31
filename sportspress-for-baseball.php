0efff1c7a8415ada152d6c1b2710be345a7858f7
Brian Miyaji <brian@themeboy.com>
2019-05-31 16:10:14 +1000

    Add support for partial innings using decimal notation

--- sportspress-for-baseball.php
+++ sportspress-for-baseball.php
@@ -5,7 +5,7 @@
  * Description: A suite of baseball features for SportsPress.
  * Author: ThemeBoy
  * Author URI: http://themeboy.com/
- * Version: 0.9.2
+ * Version: 1.0
  *
  * Text Domain: sportspress-for-baseball
  * Domain Path: /languages/
@@ -20,14 +20,19 @@ if ( ! class_exists( 'SportsPress_Baseball' ) ) :
  * Main SportsPress Baseball Class
  *
  * @class SportsPress_Baseball
- * @version	0.9.2
+ * @version	1.0
  */
 class SportsPress_Baseball {
+
+	/** @var array Performance variables to use partial innings. */
+	public $partial_inning_keys;
 
 	/**
 	 * Constructor.
 	 */
 	public function __construct() {
+		register_activation_hook( __FILE__, array( $this, 'install' ) );
+
 		// Define constants
 		$this->define_constants();
 
@@ -42,8 +47,28 @@ class SportsPress_Baseball {
 		// Define default sport
 		add_filter( 'sportspress_default_sport', array( $this, 'default_sport' ) );
 
+		// Partial innings
+		add_action( 'sportspress_meta_box_performance_details', array( $this, 'partial_innings_meta_box_setting' ) );
+		add_action( 'sportspress_process_sp_performance_meta', array( $this, 'save_partial_innings_meta_box_setting' ) );
+		add_action( 'sportspress_before_event_performance', array( $this, 'get_partial_inning_keys' ) );
+		add_filter( 'sportspress_event_performance_add_value', array( $this, 'convert_partial_innings_notation_to_real_value' ), 10, 2 );
+		add_filter( 'sportspress_event_performance_table_total_value', array( $this, 'prepare_event_performance_table_total_for_display' ), 10, 3 );
+		add_action( 'sportspress_before_player_statistics_loop', array( $this, 'get_partial_inning_keys' ) );
+		add_filter( 'sportspress_player_performance_add_value', array( $this, 'convert_partial_innings_notation_to_real_value' ), 10, 2 );
+		add_filter( 'sportspress_player_performance_table_placeholder', array( $this, 'convert_real_value_to_partial_innings_notation' ), 10, 3 );
+
 		// Include required files
 		$this->includes();
+	}
+
+	/**
+	 * Install.
+	*/
+	public static function install() {
+		$post = get_page_by_path( 'pitcher_ip', OBJECT, 'sp_performance' );
+		if ( ! $post ) return;
+
+		update_post_meta( $post->ID, 'sp_partial_innings', 1 );
 	}
 
 	/**
@@ -51,7 +76,7 @@ class SportsPress_Baseball {
 	*/
 	private function define_constants() {
 		if ( !defined( 'SP_BASEBALL_VERSION' ) )
-			define( 'SP_BASEBALL_VERSION', '0.9.2' );
+			define( 'SP_BASEBALL_VERSION', '1.0' );
 
 		if ( !defined( 'SP_BASEBALL_URL' ) )
 			define( 'SP_BASEBALL_URL', plugin_dir_url( __FILE__ ) );
@@ -83,7 +108,7 @@ class SportsPress_Baseball {
 				'name'        => 'SportsPress',
 				'slug'        => 'sportspress',
 				'required'    => true,
-				'version'     => '2.3',
+				'version'     => '2.6.16',
 				'is_callable' => array( 'SportsPress', 'instance' ),
 			),
 		);
@@ -181,6 +206,99 @@ class SportsPress_Baseball {
 	*/
 	public function default_sport() {
 		return 'baseball';
+	}
+
+	/**
+	 * Add partial innings to details meta box.
+	*/
+	public function partial_innings_meta_box_setting( $post ) {
+		$partial_innings = get_post_meta( $post->ID, 'sp_partial_innings', true );
+		?>
+		<p>
+			<strong><?php _e( 'Partial Innings', 'sportspress' ); ?></strong>
+			<i class="dashicons dashicons-editor-help sp-desc-tip" title="<?php _e( 'Convert decimals (.1 and .2) to partial innings (1/3 and 2/3)', 'sportspress' ); ?>"></i>
+		</p>
+
+		<input name="sp_partial_innings" id="sp_partial_innings" value="0" type="hidden">
+
+		<label>
+			<input name="sp_partial_innings" id="sp_partial_innings" value="1" type="checkbox" <?php checked( $partial_innings ); ?>>
+			<?php _e( 'Decimal notation for baseball', 'sportspress' ); ?>
+		</label>
+		<?php
+	}
+
+	/**
+	 * Save partial innings meta box setting.
+	*/
+	public function save_partial_innings_meta_box_setting( $post_id ) {
+		update_post_meta( $post_id, 'sp_partial_innings', sp_array_value( $_POST, 'sp_partial_innings', 0 ) );
+	}
+
+	/**
+	 * Get performance keys to use partial innings.
+	*/
+	public function get_partial_inning_keys( $columns = null ) {
+		if ( $columns == null ) {
+			// Get performance columns
+			$args = array(
+				'post_type' => 'sp_performance',
+				'numberposts' => 100,
+				'posts_per_page' => 100,
+				'orderby' => 'menu_order',
+				'order' => 'ASC',
+			);
+
+			$columns = get_posts( $args );
+		}
+
+		foreach ( $columns as $column ) {
+			$partial_innings = get_post_meta( $column->ID, 'sp_partial_innings', true );
+			if ( $partial_innings ) {
+				$this->partial_inning_keys[] = $column->post_name;
+			}
+		}
+	}
+
+	/**
+	 * Convert partial innings notation to real value.
+	*/
+	public function convert_partial_innings_notation_to_real_value( $value, $key ) {
+		if ( ! is_array( $this->partial_inning_keys ) ) {
+			$this->get_partial_inning_keys();
+		}
+
+		if ( in_array( $key, $this->partial_inning_keys ) ) {
+			list( $whole, $decimal ) = sscanf( $value, '%d.%d' );
+			if ( in_array( $decimal, array( 1, 2 ) ) ) {
+				$value = (int) $whole + (int) $decimal / 3;
+			}
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Convert real value to partial innings notation.
+	*/
+	public function convert_real_value_to_partial_innings_notation( $value, $key ) {
+		if ( ! is_array( $this->partial_inning_keys ) ) {
+			$this->get_partial_inning_keys();
+		}
+
+		if ( in_array( $key, $this->partial_inning_keys ) ) {
+			$value = floatval( $value );
+			$value = floor( $value ) + round( ( $value - floor( $value ) ) * 3 ) / 10;
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Convert event performance table total value for display using partial innings notation.
+	*/
+	public function prepare_event_performance_table_total_for_display( $value, $data, $key ) {
+		return $this->convert_real_value_to_partial_innings_notation( $value, $key );
 	}
 }
 
